2b0cdc488749a7441c7eb8a1947870970484db49
dhass421 <dhass421@gmail.com>
2020-03-02 17:32:10 -0500

    Added db connection

---
+++ controllers/usersController.js
@@ -0,0 +1,6 @@
+// @desc    Create new user
+// @route   POST /api/users
+// @access   Public
+exports.createUser = (req, res, next) => {
+    res.send('POST request');
+}
--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db')
 
 dotenv.config({ path: './config/config.env' });
 
+connectDB();
+
 const requests = require('./routes/requests');
+const users = require('./routes/users');
 
 const app = express();
 
 app.use('/api/requests', requests);
+app.use('/api/users', users);
 
 const PORT = process.env.PORT || 5000;
 
